60a30f861c6f9526172c0a36739dc5509bfd2101
Yusong Wu <1391530737@qq.com>
2018-06-01 11:23:27 +0800

    Update Vowel Count.hs

--- Vowel Count.hs
+++ Vowel Count.hs
@@ -1,8 +1,8 @@
-(-
+{-
 Return the number (count) of vowels in the given string.
 We will consider a, e, i, o, and u as vowels for this Kata.
 The input string will only consist of lower case letters and/or spaces.
--)
+-}
 
 module Codewars.Kata.Vowel where
 
