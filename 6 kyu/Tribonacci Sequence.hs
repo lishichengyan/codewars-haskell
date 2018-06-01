a9ed2f3dc79ac72d6aba6895423aa7547a75c8e0
Yusong Wu <1391530737@qq.com>
2018-06-01 11:54:09 +0800

    Create Tribonacci Sequence.hs

---
+++ 6 kyu/Tribonacci Sequence.hs
@@ -0,0 +1,13 @@
+module Tribonacci where
+
+tribonacci :: Num a => (a, a, a) -> Int -> [a]
+tribonacci (a,b,c) n = take n $ fibonacci (a,b,c)
+fibonacci :: Num a => (a,a,a) -> [a]
+fibonacci (a,b,c) = [a,b,c] ++ fibonacci ((a+b+c),(a+2*b+2*c),(2*a+3*b+4*c))
+
+{--another version}
+module Tribonacci where
+
+tribonacci :: Num a => (a, a, a) -> Int -> [a]
+tribonacci _ n | n < 1 = []
+tribonacci (a, b, c) n = a : tribonacci (b, c, a+b+c) (n-1)
